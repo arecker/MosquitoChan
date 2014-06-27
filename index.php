b30404fdbdb629c64f0499f6d784c666bb440cd1
Alex Recker <alex@reckerfamily.com>
2014-06-27 21:57:25 +0000

    hooray

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<h2>Emails in DB:</h2>
+<ul>
 <?php
 include 'connection.php';
 
-*$connection = mysql_connect($dbhost, $dbuser, $dbpass);
+$connection = mysql_connect($dbhost, $dbuser, $dbpass);
 
 // Check Connection
 if (! $connection )
@@ -10,20 +16,27 @@ if (! $connection )
 }
 
 // Build Query
-$sql = 'SELECT * FROM test';
+$sql = 'SELECT email FROM test';
 
 // Choose DB
 mysql_select_db('Mosquito');
 
 // Run query
-$returned = mysql_query($sql, $connection);
+$returned = mysql_fetch_row(mysql_query($sql, $connection));
 
 // Check for failure
 if (! $returned )
 {
         die('Could not get data: ' . mysql_error());
 }
-echo $returned;
-mysql_close($connection);*/
 
+
+for ($i=0 ; $i<count($returned) ; $i++) {
+    echo "<li>";
+    echo $returned[$i];
+    echo "</li>";
+}
 ?>
+
+</ul>
+</body>

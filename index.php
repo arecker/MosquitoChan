33c2adebbdb580c9858f86a8c4f72cacdea8b6aa
Alex Recker <alex@reckerfamily.com>
2014-06-28 12:05:44 -0500

    First form

--- index.php
+++ index.php
@@ -1,42 +1,39 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <head>
+	<title>Mosquito Chan</title>
+	<style>
+		.error { color: red;}
+	</style>
 </head>
 <body>
-<h2>Emails in DB:</h2>
-<ul>
-<?php
-include 'connection.php';
-
-$connection = mysql_connect($dbhost, $dbuser, $dbpass);
-
-// Check Connection
-if (! $connection )
-{
-        die('Could not connect: ' . mysql_error());
-}
-
-// Build Query
-$sql = 'SELECT email FROM test';
-
-// Choose DB
-mysql_select_db('Mosquito');
-
-// Run query
-$returned = mysql_fetch_row(mysql_query($sql, $connection));
-
-// Check for failure
-if (! $returned )
-{
-        die('Could not get data: ' . mysql_error());
-}
-
-
-for ($i=0 ; $i<count($returned) ; $i++) {
-    echo "<li>";
-    echo $returned[$i];
-    echo "</li>";
-}
-?>
-
-</ul>
+	<div class="banner">
+		<h1>Mosquito Chan</h1>
+		<p><em>A Place for posting</em></p>
+	</div>
+	<div class="login">
+		<h3>Register</h3>
+		<form action="index.php" method="post" onsubmit="return RegisterationPasswordsMatch()">
+			<div class="error" id="registration_error"></div>
+			<p>Username: <input name="user" type="text"></p>
+			<p>Password: <input id="register_password" name="password" type="password"></p>
+			<p>Password Confirm: <input id="register_password_confirm" type="password"></p>
+			<input type="button" onclick="submit" value="Submit">
+		</form>
+		<script type="text/JavaScript">
+			// Validate New passwords match
+			function RegistrationPasswordsMatch() {
+				var pass1 = document.getElementById('register_password').value;
+				var pass2 = document.getElementById('register_password_confirm').value;
+				if (pass1 == pass2) {
+					return true;
+				} else {
+					pass1 = '';
+					pass2 = '';
+					console.log('here');
+					document.getElementById('registration_error').innerHTML = '<p>The Passwords do not match.</p>';
+					return false;
+				}
+			}
+		</script>
+	</div>
 </body>

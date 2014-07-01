d5cd242342f5978cc1f0528511c6a1bd160bf722
Alex Recker <alex@reckerfamily.com>
2014-07-01 14:51:19 -0500

    Registration

--- index.php
+++ index.php
@@ -26,10 +26,33 @@
 		<h3>Register</h3>
 		<form action="index.php" method="post" onsubmit="return RegistrationPasswordsMatch()">
 			<div id="registration_error" class="error"></div>
-			<p>Username: <input name="user" type="text"></p>
-			<p>Password: <input id="register_password" name="password" type="password"></p>
+			<p>Username: <input name="new_user" type="text"></p>
+			<p>Password: <input id="register_password" name="new_password" type="password"></p>
 			<p>Password Confirm: <input id="register_password_confirm" type="password"></p>
 			<input type="submit" value="Submit">
 		</form>
 	</div>
 </body>
+<footer>
+	<?php
+		include('connection.php');
+
+		// Register New User
+		if ($_POST['new_user'] && $_POST['new_password']) {
+			CreateNewUser($_POST['new_user'], $_POST['new_password']);
+		}
+
+		function CreateNewUser($username, $password){
+			$connection = mysql_connect($dbhost, $dbuser, $dbpass);
+			mysql_select_db('Mosquito');
+			$sql = 'INSERT INTO user (username, password) VALUES($username, $password);'
+			$returned = mysql_query($sql, $connection);
+
+			// Check for failure
+			if (! $returned )
+			{
+				die('Could not get data: ' . mysql_error());
+			}
+		}
+	?>
+</footer>

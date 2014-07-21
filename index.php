02586b5c25f387f7cc4a0accb9459125bfd54795
Alex Recker <alex@reckerfamily.com>
2014-07-20 23:25:40 -0500

    forum

--- index.php
+++ index.php
@@ -1,52 +1,68 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Mosquito Chan</title>
+  <meta charset="UTF-8">
+  <title>Mosquito Chan</title>
 </head>
-<body>
-	<div>
-		<h1>Mosquito Chan</h1>
-	</div>
-	<div>
-		<h3>Upload</h3>
-		<form action="index.php" method="post"
-		enctype="multipart/form-data">
-			<label for="file">Filename:</label>
-			<input type="file" name="file" id="file"><br>
-			<input type="submit" name="submit" value="Submit">
-		</form>
-	</div>
-</body>
-<?php
-$allowedExts = array("gif", "jpeg", "jpg", "png");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
 
-if ((($_FILES["file"]["type"] == "image/gif")
-|| ($_FILES["file"]["type"] == "image/jpeg")
-|| ($_FILES["file"]["type"] == "image/jpg")
-|| ($_FILES["file"]["type"] == "image/pjpeg")
-|| ($_FILES["file"]["type"] == "image/x-png")
-|| ($_FILES["file"]["type"] == "image/png"))
-&& in_array($extension, $allowedExts)) {
-  if ($_FILES["file"]["error"] > 0) {
-    echo "Return Code: " . $_FILES["file"]["error"] . "<br>";
-  } else {
-    echo "Upload: " . $_FILES["file"]["name"] . "<br>";
-    echo "Type: " . $_FILES["file"]["type"] . "<br>";
-    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " kB<br>";
-    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br>";
-    if (file_exists("upload/" . $_FILES["file"]["name"])) {
-      echo $_FILES["file"]["name"] . " already exists. ";
-    } else {
-      move_uploaded_file($_FILES["file"]["tmp_name"],
-      "upload/" . $_FILES["file"]["name"]);
-      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
-    }
+<body>
+  <h1>Mosquito Chan</h1>
+
+  <p>
+    <h2>Start Thread</h2>
+    <form method="POST">
+      <input type="hidden" id="Username" name="Username" value=""/>
+      Title: <input name="newthread_title" type="text"/> <br><br>
+      Text: <br><textarea name="newthread_text" rows="6" cols="40"></textarea> <br>
+      <input type="submit">
+    </form>
+  </p>
+
+  <hr>
+<?php
+  require('connection.php');
+  $connection = mysqli_connect($DBHOST, $DBUSER, $DBPASS, "Mosquito");
+  $result = mysqli_query($connection, 'SELECT * FROM threads ORDER BY timestamp DESC');
+  mysqli_close($connection);
+  while($row = mysqli_fetch_array($result)) {
+    echo '<h2>' . $row['title'] . '</h2>';
+    echo '<p>' . $row['text'] . '</p>';
+    echo "<br>";
   }
-} else {
-  echo "Invalid file";
-}
 ?>
+</body>
+
+<footer>
+<?php
+
+  function create_new_thread($title, $text){
+    require('connection.php');
+    $connection = mysqli_connect($DBHOST, $DBUSER, $DBPASS, "Mosquito");
+    $result = mysqli_query($connection, "CALL new_thread('" . $title . "', '" . $text . "');");
+    mysqli_close($connection);
+    header('Location: /');
+  }
+  
+  // Postbacks
+  if (!empty($_POST)) {
+
+    // Honeypot
+    if (!empty($_POST['Username'])){
+      header('Location: http://google.com');
+    }
+    
+    // New Thread
+    if (!empty($_POST['newthread_title']) && !empty($_POST['newthread_text'])){
+      $title = $_POST['newthread_title'];
+      $text = $_POST['newthread_text'];
+      create_new_thread($title, $text);
+    }
+
+  }
+
+  // Clear out post data
+  unset($_POST);
+
+?>
+</footer>
 </html>
